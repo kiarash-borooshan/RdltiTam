67ed709267caceb1250e113cc6bf55cb79d8e3e2
kia <K@b.com>
2024-04-04 16:09:00 +0330

    ListView class

--- blog/url.py
+++ blog/url.py
@@ -7,6 +7,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("post_list", views.post_list, name="post_list"),
-    path("post_detail/<int:pk>/<slug:slug>", views.post_detail, name="post_detail")
+    # path("post_list", views.post_list, name="post_list"),
+    path("post_list", views.PostListView.as_view(), name="post_list"),
+    path("post_detail/<int:pk>/<slug:slug>", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 
 
@@ -8,27 +10,34 @@ def index(request):
     return HttpResponse("hi")
 
 
-def post_list(request):
-    # ol = get_object_or_404(Post) # فقط برای تک آبجکت بکار می‌رود
-    # ol = Post.published.all()
-    # ol = Post.objects.y(2020)
-    # ol = Post.objects.filter(publish__year="2024")
-    # ol = Post.objects.filter(status="published")
-    ol = Post.objects.all()
+# def post_list(request):
+#     # ol = get_object_or_404(Post) # فقط برای تک آبجکت بکار می‌رود
+#     # ol = Post.published.all()
+#     # ol = Post.objects.y(2020)
+#     # ol = Post.objects.filter(publish__year="2024")
+#     # ol = Post.objects.filter(status="published")
+#     ol = Post.objects.all()
+#
+#     pagination = Paginator(ol, 2)
+#     page = request.GET.get("page")
+#
+#     try:
+#         posts = pagination.page(page)
+#     except EmptyPage:
+#         posts = pagination.page(1)
+#     except PageNotAnInteger:
+#         posts = pagination.page(pagination.num_pages)
+#
+#     return render(request,
+#                   "post/post_list.html",
+#                   {"posts": posts})
 
-    pagination = Paginator(ol, 2)
-    page = request.GET.get("page")
 
-    try:
-        posts = pagination.page(page)
-    except EmptyPage:
-        posts = pagination.page(1)
-    except PageNotAnInteger:
-        posts = pagination.page(pagination.num_pages)
-
-    return render(request,
-                  "post/post_list.html",
-                  {"posts": posts})
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = "posts"
+    paginate_by = 2
+    template_name = "post/post_list.html"
 
 
 def post_detail(request, pk, slug):
